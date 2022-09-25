68ca85a705ff23f5c69d3dee68cceccde1a4b1bc
Bjorn Jakobsen <106143706+Ukjeent@users.noreply.github.com>
2022-09-25 07:40:29 +0200

    Animation update

--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@
 
 const fadeIn = document.querySelectorAll('.fade-in');
 
-fadeIn.forEach(element => {
-    const duration = Math.random() * 2;
-    const delay = Math.random() * 2;
-    element.style.animationDuration = `${duration}s`;
+// fadeIn.forEach(element => {
+//     const duration = Math.random() * 2;
+//     const delay = Math.random() * 2;
+//     element.style.animationDuration = `${duration}s`;
+//     element.style.animationDelay = `${delay}s`;
+// });
+
+fadeIn.forEach((element, index) => {
+    const delay = (index + 5) / 15;
     element.style.animationDelay = `${delay}s`;
 });
